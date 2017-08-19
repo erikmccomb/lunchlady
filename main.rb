a0cc7bd57ee80841192488d6cd717931a8dc98ae
Erik McComb <erik@erikmccomb.com>
2017-08-19 10:50:48 -0600

    all basic requirements met. added aggressive raccoons

--- main.rb
+++ main.rb
@@ -1,5 +1,4 @@
 # Basic Objectives:
-# - computer totals lunch items and displays total
 
 # Bonus Objectives:
 # - the user can choose as many "add-on" items as they want before getting total
@@ -14,25 +13,37 @@
 require 'pry'
 require 'colorize'
 
-@main_dish = [ {dish: 'Burger', cost: 6.00, calories: 700},
-              {dish: 'Bratwurst', cost: 4.00, calories: 400},
-              {dish: 'Vegan Soy-Bison', cost: 7.00, calories: 700}
+@main_dish = [ {dish: 'Burger', cost: 5.99, calories: 700},
+              {dish: 'Bratwurst', cost: 3.99, calories: 400},
+              {dish: 'Vegan Soy-Bison', cost: 7.25, calories: 650}
 ]
 
-@side_dish = [ {dish: 'Fries', cost: 2.00, calories: 300},
-              {dish: 'Mac n Cheese', cost: 3.00, calories: 350},
-              {dish: 'Dressed Baked Potato', cost: 2.50, calories: 400}
+@side_dish = [ {dish: 'Fries', cost: 2.25, calories: 300},
+              {dish: 'Mac n Cheese', cost: 2.50, calories: 250},
+              {dish: 'Dressed Baked Potato', cost: 3.35, calories: 400}
 ]
  
-@dessert_dish = [ {dish: 'Cake', cost: 2.00, calories: 300},
-            {dish: 'Pudding', cost: 1.00, calories: 350},
-            {dish: 'Literally just a bag of sugar', cost: 1.00, calories: 1500}
+@dessert_dish = [ {dish: 'Brownie', cost: 2.50, calories: 250},
+            {dish: 'Vanilla Shake', cost: 3.00, calories: 400},
+            {dish: 'Literally a bag of sugar', cost: 1.00, calories: 1500}
 ]
 
-@order=[]
-@price=[]
-@calories=[]
+@order = []
+@dishes = []
+@price = []
+@calories = []
 
+def main_push
+  @dishes.push(@order[0][:dish])
+  @price.push(@order[0][:cost])
+  @calories.push(@order[0][:calories])
+end
+
+def side_push
+  @dishes.push(@order[0][:dish])
+  @price.push(@order[0][:cost])
+  @calories.push(@order[0][:calories])
+end
 
 def first_course
   @main_dish.each_with_index do |key, index|
@@ -42,17 +53,13 @@ def first_course
   case input
   when '1'
     @order.push(@main_dish[0])
-    @price.push(@order[0][:cost])
-    @calories.push(@order[0][:calories])
-    binding.pry
+    main_push
   when '2'
     @order.push(@main_dish[1])
-    @price.push(@order[0][:cost])
-    @calories.push(@order[0][:calories])
+    main_push
   when '3'
     @order.push(@main_dish[2])
-    @price.push(@order[0][:cost])
-    @calories.push(@order[0][:calories])
+    main_push
   end
 end
 
@@ -64,10 +71,19 @@ def side_dish
   case input
   when '1'
     @order.push(@side_dish[0])
+    @dishes.push(@side_dish[0][:dish])
+    @price.push(@side_dish[0][:cost])
+    @calories.push(@side_dish[0][:calories])
   when '2'
     @order.push(@side_dish[1])
+    @dishes.push(@side_dish[1][:dish])
+    @price.push(@side_dish[1][:cost])
+    @calories.push(@order[1][:calories])
   when '3'
     @order.push(@side_dish[2])
+    @dishes.push(@side_dish[2][:dish])
+    @price.push(@side_dish[2][:cost])
+    @calories.push(@side_dish[2][:calories])
   end
 end
 
@@ -79,37 +95,43 @@ def side_loop
     if c == 2
       break
     end
-    p 'Tasty! and for your second side?'
+    puts 'Tasty! and for your second side?'
+    puts ''
   end
 end
 
 def to_go
   @order.each_with_index do |key, index|
-    puts "#{index + 1}) #{key[:dish]}  (#{key[:calories]} cal.) ....... $#{key[:cost]}".colorize(:yellow)
+    puts "#{index + 1}) #{key[:dish]}  (#{key[:calories]} cal.) ....... $#{key[:cost]}".colorize(:light_blue)
   end
 end
 
-def calories
-  total = 0
-  @order.values
-  binding.pry
-end
+
+# def calories
+#   # count calories of items ordered
+# end
 
 def cash
+  puts "The total calorie count for you meal is #{@calories.inject(:+)} Cal."
+  puts "Your grand total today will be $#{@price.inject(:+)}."
 end
 
+
 def main_menu
-  p 'Welcome to McCombs!'
-  p 'We are currently serving dinner; What would you like?'
+  puts 'Welcome to McCombs!'
+  puts 'We are currently serving lunch; What would you like?'
   first_course
-  p 'Excellent choice! That comes with two sides. Which would you like?'
+  puts ''
+  puts 'Excellent choice! That comes with two sides. Which would you like?'
   side_loop
+  puts ''
   puts '-----------------------------------'
-  p 'Ok, so you ordered the following:'
+  puts 'Ok, so you ordered the following:'
   to_go
   puts '-----------------------------------'
-  p 'May I suggest taking your order to go?'
-  p 'The raccoons have taken the patio and the indoor dining area.'
+  cash
+  puts 'May I suggest taking your order to go?'
+  puts 'The raccoons are rather aggressive.'
 end
 
 
